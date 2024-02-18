dca2c3e74b643e2c9a5bcd31ab05b41f1384229e
abiodun603 <abioduncsc@gmail.com>
2024-02-18 21:07:08 +0100

    fist commit

---
+++ backend/src/index.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const mongoose = require('mongoose');
+mongoose.connect(
+  'mongodb+srv://abiodun_mastery:Testing123@cluster0.jupgc1f.mongodb.net/?retryWrites=true&w=majority'
+);
+
+const db = mongoose.connection;
+
+db.on('error', console.log.bind(console, 'connection error'));
+db.once('open', function (callback) {
+  console.log('connection succeeded');
+});
+
+const app = express();
+app.use(
+  cors({
+    // origin: 'http://127.0.0.1:5500', // Allow requests from this origin
+    methods: ['GET', 'POST'], // Allow these HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Allow these headers
+    optionsSuccessStatus: 200,
+  })
+);
+const path = require('path');
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+// Serve static files from the frontend folder
+app.use(express.static(path.join(__dirname, '../frontend')));
+
+app.post('/sign_up', function (req, res) {
+  console.log(req.body);
+  var name = req.body.name;
+  var email = req.body.email;
+  var phone = req.body.phone;
+  var timestamp = new Date();
+
+  var data = {
+    name: name,
+    email: email,
+    phone: phone,
+    timestamp: timestamp,
+  };
+  db.collection('details').insertOne(data, function (err, collection) {
+    if (err) throw err;
+    console.log('Record inserted Successfully');
+  });
+
+  return res.redirect('frontend/index.html');
+});
+
+app
+  .get('/', function (req, res) {
+    res.set({
+      'Access-control-Allow-Origin': '*',
+    });
+    return res.redirect('index.html');
+  })
+  .listen(3000);
+
+console.log('server listening at port 3000');
---
+++ frontend/js/tailwind.config.js
@@ -0,0 +1,32 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        clifford: '#299F32',
+        textColor: "#B7B8BB",
+        textBlack: "#000000",
+        green: "#299F32",
+        darkgray: "#111111",
+        darklightgray: "#2d2d2d",
+        lightgreen: "#51C728",
+        yellow: "#EB8C00",
+        noteshadow: "0px 0px 30px rgba(0, 0, 0, 0.1)",
+        buttonshadow: "0px 0px 30px rgba(41, 159, 50, 0.3)"
+
+      },
+      fontSize: {
+        h1fontsize: " 1.5rem",
+        h2fontsize: "1.25rem",
+        h3fontsize: "1rem",
+        normalfontsize: "0.938rem",
+        smallfontsize: "0.813rem",
+        smallerfontsize: "0.75rem"
+      },
+      listStyleType: {
+        none: 'none',
+        square: 'square',
+        roman: 'upper-roman',
+      }
+    }
+  }
+}
